94130a98714c4aaa8e66a394f0700ccdfce30d04
CultivatedCurt <92338659+CultivatedCurt@users.noreply.github.com>
2023-04-02 09:36:30 -0600

    remove react, simplify

--- main.js
+++ main.js
@@ -1,4 +1,46 @@
-import App from "./App.js";
+let scene, camera, renderer, cube;
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(React.createElement(App), rootElement);
+function init() {
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = 5;
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.getElementById("container").appendChild(renderer.domElement);
+
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshStandardMaterial({ color: "orange" });
+  cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  const light = new THREE.PointLight(0xffffff, 1);
+  light.position.set(10, 10, 10);
+  scene.add(light);
+
+  window.addEventListener("resize", onWindowResize, false);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+init();
+animate();
